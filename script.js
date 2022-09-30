d07ee935bc0693c34d85973192c975b5459b5d86
PeterEKB <109247624+PeterEKB@users.noreply.github.com>
2022-09-30 17:11:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* * * * APIs * * * */
 class APIs {
   rndUser = async () => {
-    const bUrl = 'https://randomuser.me/api'
+    const bUrl = 'https://randomuser.me/api';
 
     return await fetch(`${bUrl}`).then((res) => res.json());
   };
@@ -11,120 +11,116 @@ class APIs {
 
 class View {
   disArea = document.querySelector('view');
-  dob = this.disArea.querySelector('dob')
-  lBar = document.querySelector('loading-bar')
-  progress = document.querySelector('progress')
-  refresh = document.querySelector('refresh')
+  dob = this.disArea.querySelector('dob');
+  lBar = document.querySelector('loading-bar');
+  progress = document.querySelector('progress');
+  refresh = document.querySelector('refresh');
 }
 
 /* * * * Model * * * */
 
 class Model {
   loaded = 0;
-  
+
   constructor(data) {
     this.apis = data.Apis || data.Api || data.apis || data.api;
     this.view = data.Views || data.View || data.views || data.view;
-    this.#listeners
+    this.#listeners;
   }
 
   async getUsers(n = 1, cb) {
-    let users = [], x = 0
-    this.view.lBar.style.display = `block`
-    while(x < n){
+    let users = [],
+      x = 0;
+    this.view.lBar.style.display = `block`;
+    while (x < n) {
       this.res = this.apis.rndUser();
-      users.push(await this.res)
-      this.loaded = x
-      this.total = n
-      this.disUsers = `loaded ${x} of ${n}`
-      ++x
-      this.view.progress.style.width = `${x/n*100}%`
+      users.push(await this.res);
+      this.loaded = x;
+      this.total = n;
+      this.disUsers = `loaded ${x} of ${n}`;
+      ++x;
+      this.view.progress.style.width = `${(x / n) * 100}%`;
     }
-    this.view.lBar.style.display = `none`
-    return users
+    this.view.lBar.style.display = `none`;
+    return users;
   }
-  set disUsers (c){
-    const v = this.view
+  set disUsers(c) {
+    const v = this.view;
 
-    v.disArea.innerHTML = c
+    v.disArea.innerHTML = c;
   }
-  format (n){
+  format(n) {
     const res = `
     <card>
       <c-image>
         <img src="${n.picture.thumbnail}" />
       </c-image>
       <profile>
-        <name>${n.name.first,n.name.last}</name>
+        <name>${(n.name.first, n.name.last)}</name>
         <email>${n.email}</email>
         <phone>${n.phone}</phone>
         <dob><i class="hidden">${n.dob.date}</i></dob>
       </profile>
     </card>
-    `
-    return res
+    `;
+    return res;
   }
-  get #listeners(){
-    this.view.disArea.addEventListener('click', e => {
+  get #listeners() {
+    this.view.disArea.addEventListener('click', (e) => {
       e.target.tagName.toLowerCase() == 'i'
-      ?e.target.classList.toggle('hidden')
-      :''
-    })
-    this.view.refresh.addEventListener('click', e => {
-      App.refresh
-    })
+        ? e.target.classList.toggle('hidden')
+        : '';
+    });
+    this.view.refresh.addEventListener('click', (e) => {
+      App.refresh;
+    });
   }
 }
 
 /* * * * Controller * * * */
 
 class Controller {
-  users = []
-  loaded = 0
+  users = [];
+  loaded = 0;
   constructor(model) {
     [this.model, this.view] = [model];
   }
-  get init(){
+  get init() {
     const m = this.model,
-    v = m.view,
-    n = 20,
-    usr = m.getUsers(n)
+      v = m.view,
+      n = 20,
+      usr = m.getUsers(n);
 
-    usr.then((res)=>{
-      res.forEach((u)=>{
-        this.format = u.results[0]
-      })
-      m.disUsers = this.users
-      // this.users.forEach(u =>{
-      //   v.disArea.innerHTML += u
-      // })
-    })
+    usr.then((res) => {
+      res.forEach((u) => {
+        this.format = u.results[0];
+      });
+      m.disUsers = this.users.join('');
+    });
   }
-  get refresh (){
-    this.users = []
-    console.log(this.users)
-    this.init
+  get refresh() {
+    this.users = [];
+    console.log(this.users);
+    this.init;
   }
   set format(s) {
     const m = this.model,
-    f = m.format(s)
-    console.log(s)
+      f = m.format(s);
+    console.log(s);
 
-    this.users.push(f)
+    this.users.push(f);
   }
   set disUsers(s) {
-    const d = this.model
+    const d = this.model;
 
-    d.disUsers(s)
+    d.disUsers(s);
   }
-  set loaded(n){
-
-  }
+  set loaded(n) {}
 }
 
 const api = new APIs(),
-view = new View(),
-model = new Model({ api, view }),
-App = new Controller(model);
+  view = new View(),
+  model = new Model({ api, view }),
+  App = new Controller(model);
 
-App.init
+App.init;
